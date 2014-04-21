1e2a4840c6c420311a2771fd06c7e9ff61edc83a
Anatol Broder <anatol@penibelst.de>
2014-04-21 23:44:35 +0200

    Expose javascripts to the settings

--- noitadnouf/javascript.js
+++ noitadnouf/javascript.js
@@ -1,3 +1,11 @@
 ---
-layout: javascript
 ---
+
+{% include noitadnouf/foundation/js/vendor/jquery.js %}
+{% include noitadnouf/foundation/js/foundation.min.js %}
+
+$(document).foundation();
+
+{% for javascript in site.noitadnouf.javascripts %}
+  {% include javascript %}
+{% endfor %}
